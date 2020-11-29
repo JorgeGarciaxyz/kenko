7fef289a402641aa5d9e909eef598c37006a4cd0
Jorge Garcia <jorge.mgg94@gmail.com>
2020-11-28 20:07:39 -0600

    Test[home]: Add basic home request spec

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -3,7 +3,19 @@
 require "rails_helper"
 
 RSpec.describe "Homes", type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+  end
+
   describe "GET /index" do
-    it "returns http success"
+    before do
+      get "/"
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
   end
 end
