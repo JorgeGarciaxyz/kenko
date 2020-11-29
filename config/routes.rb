20603f8db3fab4f74b192dc2463aaf186b47f600
Jorge Garcia <jorge.mgg94@gmail.com>
2020-11-28 19:12:57 -0600

    Feat[users]: Initial Devise User setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root "home#index"
+
+  devise_for :users
+
   get "home/index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
