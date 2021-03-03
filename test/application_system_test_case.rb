6b14b89da379170ffc0f0c863738f9d2809489de
Jorge Garcia <jorge.mgg94@gmail.com>
2021-03-02 22:17:27 -0600

    Test[system]: Use headless chrome driver

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require "minitest/rails/capybara"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome
 
   register_spec_type(self) do |desc, *addl|
     addl.include? :system
