2bb8ae146b4f8f0e92d6f96caa9c5f5ce8dc577e
Jorge Garcia <jorge.mgg94@gmail.com>
2021-04-29 19:07:32 -0500

    Feat: Create buttons helper

---
+++ app/helpers/buttons_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ButtonsHelper
+  def dark_blue_button
+    "focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg"
+  end
+
+  def red_button
+    "focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-red-500 hover:bg-red-600 hover:shadow-lg"
+  end
+end
